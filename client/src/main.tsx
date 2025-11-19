d48f614034bd369039a2bb1d16bcbdac429a9027
rub3nnn <rubenmoraleszgz@gmail.com>
2025-11-20 00:10:38 +0100

    first commit

---
+++ client/src/main.tsx
@@ -0,0 +1,12 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "@/App";
+import { ThemeProvider } from "@/components/theme-provider";
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
+      <App />
+    </ThemeProvider>
+  </StrictMode>
+);
